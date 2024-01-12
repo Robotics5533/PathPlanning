0413f9dc21ff321005c45a02deae8336b65d8ae4
5533Robotics <5533ehprobotics@gmail.com>
2024-01-11 20:03:09 -0500

    thing

--- pathplanner.py
+++ pathplanner.py
@@ -1,22 +1,36 @@
 import bpy
+import json
+import os
 
 obj = bpy.context.object
-
+file_name = obj.name_full
+filepath = "C:\\Users\\Electric Horsepower\\Desktop"
 keyframes = []
+seperate = os.seperator
+unit = 1000
 
-
-def grab_data(scene):
+def grab_data(s):
+    scene = bpy.context.scene
+    
     keyframe = {
-        "frame_time": bpy.context.scene.frame_float,
-        "unit": 1000,
+        "frame_time": scene.frame_float,
         "position": {
             "x": obj.location.x,
             "y": obj.location.y,
             "z": obj.location.z,
+            
         },
     }
-
+    print(keyframe)
     keyframes.append(keyframe)
+    if scene.frame_float == scene.frame_end:
+        with open(os.path.join(filepath, filename), "w") as json_out:
+            json.dump({"keyframes":keyframes,"unit":unit}, json_out)
 
 
+
+
+
+
+bpy.app.handlers.frame_change_post.clear()
 bpy.app.handlers.frame_change_post.append(grab_data)
